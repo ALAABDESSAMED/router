e4b335ac1a84a497365426d6ab880db488da148b
ALAABDESSAMED <93202870+ALAABDESSAMED@users.noreply.github.com>
2022-01-10 12:19:57 +0100

    Add files via upload

---
+++ src/components/Movies.js
@@ -0,0 +1,71 @@
+const Movies = [
+    {
+      Title: "One Piece",
+      Description:
+        " Monkey D. Luffy Monkey D. Luffy (モンキー・Ｄ・ルフィ, Monkī Dī Rufi?), surnommé de manière récurrente Luffy au chapeau de paille (麦わらのルフィ, Mugiwara no Rufi?), est un personnage de fiction et principal protagoniste de la franchise japonaise One Piece créée par Eiichirō Oda. Il est intronisé en tant que garçon ayant un corps élastique, une faculté qu'il a développée après avoir mangé un fruit du démo",
+      Rating: 5,
+      PosterURL:
+        "https://static.displate.com/857x1200/displate/2020-04-15/ea612de29ddce6924b39e9fe90745d5e_ccc8885e94d67b8e7badd0c98157539d.jpg",
+    },
+    {
+      Title: "Spongebob",
+      Description:
+        "SpongeBob SquarePants (also simply referred to as SpongeBob) is an American animated comedy television series created by marine science educator and animator Stephen Hillenburg for Nickelodeon. The series chronicles the adventures of the title character and his aquatic friends in the fictional underwater city of Bikini Bottom.",
+      Rating: 5,
+      PosterURL:
+        "https://m.media-amazon.com/images/M/MV5BNDUwNjBkMmUtZjM2My00NmM4LTlmOWQtNWE5YTdmN2Y2MTgxXkEyXkFqcGdeQXRyYW5zY29kZS13b3JrZmxvdw@@._V1_QL75_UX500_CR0,0,500,281_.jpg",
+    },
+    {
+      Title: "Naruto Uzumaki",
+      Description:
+        "Naruto (ナルト?) est un shōnen manga écrit et dessiné par Masashi Kishimoto. Naruto a été prépublié dans l'hebdomadaire Weekly Shōnen Jump de l'éditeur Shūeisha entre septembre 1999 et novembre 2014. La série a été compilée en 72 tomes.",
+      Rating: 4,
+      PosterURL:
+        "https://cdn2.unrealengine.com/fortnite-naruto-sasuke-sakura-and-kakashi-outfits-1920x1080-03fb4a1e5fbc.jpg",
+    },
+    {
+      Title: "Slam Dunk",
+      Description:
+        "Slam Dunk (スラムダンク, Suramu Danku?) est un manga de Takehiko Inoue traitant du basket-ball. Il a été prépublié dans le magazine Weekly Shōnen Jump de l'éditeur Shūeisha entre 1990 et 1996 et a été compilé en un total de 31 tomes. La version française est éditée en intégralité par Kana.",
+      Rating: 4,
+      PosterURL:
+        "https://imgsrv2.voi.id/NiAtBvQP8ByrAcLzg5I92FLQdXBr-QUP7Ud2--oLJtE/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy83NjAxMi8yMDIxMDgxNTA2MzUtbWFpbi5jcm9wcGVkXzE2Mjg5ODQxNDkuanBn.jpg",
+    },
+    {
+      Title: "Dragon Ball",
+      Description:
+        "Dragon Ball est un manga d'Akira Toriyama librement inspiré de Sun Wukong, personnage d'un roman chinois de la fin du XVIᵉ siècle, La Pérégrination vers l'Ouest de Wu Cheng'en. Il est prépublié dans le magazine Weekly Shōnen Jump de 1984 à 1995 et édité par Shūeisha en 42 volumes reliés de 1985 à 1995",
+      Rating: 5,
+      PosterURL:
+        "https://cherry.img.pmdstatic.net/fit/https.3A.2F.2Fimg.2Egaming.2Egentside.2Ecom.2Fs3.2Ffrgsg.2Fmanga.2Fdefault_2021-09-20_948b6861-2dc1-4ba5-9af9-56eb2bda4ebf.2Epng/1200x675/quality/80/dragon-ball-fans-etude.jpg",
+    },
+    {
+      Title: "Détective Conan",
+      Description:
+        "Détective Conan est une série japonaise de mangas créée par Gōshō Aoyama. La série débute le 19 janvier 1994 avec une première publication dans le magazine hebdomadaire Weekly Shōnen Sunday, et recense 100 volumes commercialisés en octobre 2021 au Japon",
+  
+      Rating: 3,
+      PosterURL:
+        "https://fusion.molotov.tv/arts/i/446x588/Ch8SHQoURCMgAQCONhyIIOj28E7mveb8W24SA2pwZxgB/jpg",
+    },
+    {
+        Title: "Pokémon",
+        Description:
+          "Pokémon (ポケモン, Pokemon?), connu sous son nom original Pocket Monsters (ポケットモンスター, Poketto Monsutā?, littéralement Monstres de poche), est une série télévisée d'animation japonaise basée sur la série de jeux vidéo Pokémon et sur une partie de la franchise Pokémon. Réalisée par le studio tokyoïte OLM et imaginée par Satoshi Tajiri, Junichi Masuda et Ken Sugimori",
+    
+        Rating: 3,
+        PosterURL:
+          "https://www.pokepedia.fr/images/thumb/d/d1/Pikachu_de_Sacha.png/1200px-Pikachu_de_Sacha.png",
+      },
+      {
+        Title: "Hunter",
+        Description:
+          "Hunter × Hunter (ハンターハンター, Hantā Hantā?) est un shōnen manga écrit et dessiné par Yoshihiro Togashi. Il est pré-publié depuis mars 1998 dans l'hebdomadaire Weekly Shōnen Jump de l'éditeur Shūeisha, et a été compilé en trente-six tomes au 4 octobre 2018. En mai 2013, le manga s'est vendu à plus de 65 millions d'exemplaires au Japon. La version française est publiée aux éditions Kana, avec trente-six tomes sortis au 3 mai 2019.",
+    
+        Rating: 3,
+        PosterURL:
+          "https://fr.web.img5.acsta.net/pictures/19/08/01/09/52/4803203.jpg",
+      },
+  ];
+  
+  export default Movies;
